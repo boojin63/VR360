f7a5ce9cb6e0a1359bd8b946bddf7700b450dc11
boojin <boojin63@gmail.com>
2024-10-11 19:25:53 +0900

    사이드바 수정

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,33 +1,54 @@
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../Assets/Sidebar.css';
 
 const Sidebar = () => {
-    const [toggle, setToggle] = useState(false); 
-    const navigate = useNavigate(); 
+    const [toggle, setToggle] = useState(false);
+    const [themeOpen, setThemeOpen] = useState(false);
+    const navigate = useNavigate();
+
 
     const onIconClick = useCallback(() => {
-        setToggle((prev) => !prev); 
+        setToggle((prev) => !prev);
     }, []);
 
-    const ToBridge = () =>{
-        navigate('/');
-    }
 
-    const ToPark = () =>{
+    const onThemeClick = useCallback(() => {
+        setThemeOpen((prev) => !prev);
+    }, []);
+
+    const toBridge = () => {
+        navigate('/');
+    };
+
+
+    const toPark = () => {
         navigate('/Park');
-    }
+    };
 
     return (
         <div>
             <div className={`SidebarContainer ${toggle ? 'open' : ''}`}>
                 <div className='SidebarContent'>
-                    <div className="SideBtn1" onClick={ToBridge}> 월영교 </div>
-                    <div className="SideBtn2" onClick={ToPark}> 물길공원 </div>
+                    <div className='SideBtn1' onClick={onThemeClick}>
+                        테마
+                    </div>
+
+                    {themeOpen && (
+                        <div className='ThemeSubMenu'>
+                            <div className="SideBtn3" onClick={toBridge}>- 월영교</div>
+                            <div className="SideBtn4" onClick={toPark}>- 물길공원</div>
+                        </div>
+                    )}
+
+                    <div className='SideBtn2' style={{ marginTop: themeOpen ? '5px' : '10px' }}>
+                        공지사항
+                    </div>
                 </div>
             </div>
+
             <div className='MenuIcon'>
                 <FontAwesomeIcon
                     className={`MenuIcon ${toggle ? 'open' : ''}`}
@@ -37,6 +58,6 @@ const Sidebar = () => {
             </div>
         </div>
     );
-}
+};
 
 export default Sidebar;
